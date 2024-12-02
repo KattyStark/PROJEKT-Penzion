d8aabae960bfe38fc280dd7f7feb01b73bd3b3a7
Kateryna Pryhkodko <kateprihodko91@icloud.com>
2024-12-03 00:09:26 +0100

    Penzion

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy('images');
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy('css');
+	// ...necháme, co už v konfiguraci je, a přidáme:
+	return {
+		// možné formáty šablon
+		templateFormats: ['njk', 'html', 'md'],
+		// jako šablonovací jazyk zvolíme Nunjucks`
+		markdownTemplateEngine: 'njk',
+		htmlTemplateEngine: 'njk',
+		dataTemplateEngine: 'njk'
+	};
+};
